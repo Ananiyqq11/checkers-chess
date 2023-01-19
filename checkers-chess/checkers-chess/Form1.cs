dfb05efc2b6332c345a0675dc5b720d0cfa6ab08
Ananiya <ananiya123416@gmail.com>
2023-01-19 23:13:09 +0300

    creates an empty 8 by 8 chess board

--- checkers-chess/checkers-chess/Form1.cs
+++ checkers-chess/checkers-chess/Form1.cs
@@ -16,5 +16,43 @@ namespace checkers_chess
         {
             InitializeComponent();
         }
+
+        int n;
+        PictureBox[,] P;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            n = 8;
+            P = new PictureBox[n, n];
+            int left = 2, top = 2;
+            Color[] colors = new Color[] { Color.White, Color.Black };
+            for (int i = 0; i < n; i++)
+            {
+                left = 2;
+                if (i % 2 == 0)
+                {
+                    colors[0] = Color.White;
+                    colors[1] = Color.Black;
+                }
+                else
+                {
+                    colors[1] = Color.White;
+                    colors[0] = Color.Black;
+                }
+
+                for (int j = 0; j < n; j++)
+                {
+                    P[i, j] = new PictureBox();
+                    P[i, j].BackColor = colors[(j % 2 == 0) ? 1 : 0];
+                    P[i, j].Location = new Point(left, top);
+                    P[i, j].Size = new Size(60, 60);
+                    left += 60;
+                    panel1.Controls.Add(P[i, j]);
+
+                }
+                top += 60;
+            }
+
+        }
     }
 }
